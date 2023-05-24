32b45bb7c579e4c995df3d90051d6c4ce2fa0353
nit-x <132815834+nit-x@users.noreply.github.com>
2023-05-25 00:30:21 +0530

    Create BufferedReaderDemo.java

---
+++ java/BufferedReaderDemo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class BufferedReaderDemo
+{
+ public static void main(String[] args) throws Exception
+  {
+   BufferedReader b= new BufferedReader(new InputStreamReader(System.in));
+   System.out.print("Enter a String: ");
+   String s=b.readLine();
+   System.out.println("The entered STRING is: " + s);
+  }
+}
