26afc518a3a86db62a975f20a61661ba97e586c8
nit-x <132815834+nit-x@users.noreply.github.com>
2023-11-05 13:48:55 +0530

    Create 1-Square-Area-Perimeter.py

---
+++ python/1-Square-Area-Perimeter.py
@@ -0,0 +1,7 @@
+side = int(input("Enter the length of side of square: "))
+
+area = side**2
+perimeter = 4 * side
+
+print("The area of square is", area, "sq.units") 
+print("The perimeter of square is", perimeter, "units")
