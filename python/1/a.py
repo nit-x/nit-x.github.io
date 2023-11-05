0be0c56614528f5ef5fa73077ad7ce759eac30f0
nit-x <132815834+nit-x@users.noreply.github.com>
2023-11-05 13:49:30 +0530

    Delete python/1 directory

--- python/1/a.py
+++
@@ -1,7 +0,0 @@
-side = int(input("Enter the length of side of square: "))
-
-area = side**2
-perimeter = 4 * side
-
-print("The area of square is", area, "sq.units") 
-print("The perimeter of square is", perimeter, "units")
